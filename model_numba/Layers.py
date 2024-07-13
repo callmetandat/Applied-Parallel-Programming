44daf116bd73b4614f447a8f48b9d29922143778
huyquoc12 <nguyenhoangquochuy12122002@gmail.com>
2024-07-13 17:11:53 +0700

    Update Tile Shared Convolution
    Load Weight to shared memory and allocate d_ouput_img in cpu instead of create np.array and copy to device

--- model_numba/Layers.py
+++ model_numba/Layers.py
@@ -3,7 +3,7 @@ import numba
 import numpy as np
 import math
 
-BLOCKSIZE = 32
+BLOCKSIZE = 16
 KERNEL_SIZE = 3
 OUTPUT_TILE_SIZE = BLOCKSIZE - KERNEL_SIZE + 1
 INPUT_TILE_SIZE = BLOCKSIZE
@@ -25,22 +25,30 @@ def tileSharedConv2D_kernel(img, out_img, weight, bias, channel_in: int, channel
     # Allocate shared memory for one input channel
     sharedImg = cuda.shared.array(shape=(BLOCKSIZE, BLOCKSIZE), dtype=img.dtype)
 
+    # Allocate shared memory for kernel of convolution
+    sharedWeight = cuda.shared.array(shape=(KERNEL_SIZE, KERNEL_SIZE), dtype=weight.dtype)
+
     outPixel = bias[out_channel_idx]
 
     isOutputPixel = shared_col < OUTPUT_TILE_SIZE and shared_row < OUTPUT_TILE_SIZE and col_out < img.shape[3] and row_out < img.shape[2]
 
     for in_channel_idx in range(channel_in):
+        #Copy kernel to shared memory
+        if shared_col < KERNEL_SIZE and shared_row < KERNEL_SIZE:
+            sharedWeight[shared_row][shared_col] = weight[out_channel_idx, in_channel_idx, shared_row, shared_col]
+
+        #Copy img tile to shared memory
         if row_in >= 0 and row_in < img.shape[2] and col_in >= 0 and col_in < img.shape[3]:
             sharedImg[shared_row, shared_col] = img[batch_idx, in_channel_idx, row_in, col_in]
         else:
             sharedImg[shared_row, shared_col] = 0.0
-
         cuda.syncthreads()
 
         if isOutputPixel:
-            for r in range(KERNEL_SIZE):
-                for c in range(KERNEL_SIZE):
-                    outPixel += weight[out_channel_idx, in_channel_idx, r, c] * sharedImg[shared_row + r, shared_col + c]
+            for index in range(KERNEL_SIZE * KERNEL_SIZE):
+                row = index // KERNEL_SIZE
+                col = index % KERNEL_SIZE
+                outPixel += sharedWeight[row, col] * sharedImg[shared_row + row, shared_col + col]
 
         cuda.syncthreads()
 
@@ -56,10 +64,8 @@ def Convolution2D_GPU(img, weight, bias):
     threadPerBlock = (BLOCKSIZE, BLOCKSIZE)
     blockPerGrid = (math.ceil(IMG_WIDTH / OUTPUT_TILE_SIZE), math.ceil(IMG_HEIGHT / OUTPUT_TILE_SIZE), BATCH_SIZE * OUT_CHANNEL)
 
-    out_img = np.zeros((BATCH_SIZE, OUT_CHANNEL, IMG_HEIGHT, IMG_WIDTH), dtype=np.float32)
-
     d_img = cuda.to_device(img)
-    d_out_img = cuda.to_device(out_img)
+    d_out_img = cuda.device_array(shape=(BATCH_SIZE, OUT_CHANNEL, IMG_HEIGHT, IMG_WIDTH), dtype= img.dtype)
     d_weight = cuda.to_device(weight)
     d_bias = cuda.to_device(bias)
 
@@ -68,6 +74,14 @@ def Convolution2D_GPU(img, weight, bias):
     out_img = d_out_img.copy_to_host()
 
     return out_img
+
+@cuda.jit
+def split_channel_tileSharedConv2D_kernel(img, out_img, weight, bias, channel_in: int, channel_out: int, batch_size: int, channel_per_thread: int):
+    pass
+
+
+def Split_Channel_TileSharedConv2D_GPU(img, out_img, weight, bias, channel_per_thread: int = 8):
+    pass
     
 
 
